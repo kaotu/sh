08308e55aa6f878c71c1d13f06d4d7d6cbf1f3c7
kaotu <32597649+kaotu@users.noreply.github.com>
2017-12-24 23:33:15 +0700

    Delete port1.sh

--- port1.sh
+++
@@ -1,8 +0,0 @@
-#./bin/bash
-
-sudo systemctl start firewalld
-sudo firewall-cmd --add-port=80/tcp --permanent
-sudo firewall-cmd --add-port=3000/tcp --permanent
-sudo firewall-cmd --reload
-sudo firewall-cmd --list-port
-

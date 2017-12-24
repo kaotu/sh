5a478d2bd8606e69739ea35ae19e35ae9bbf94b1
harit suttisaksri <haritsuttisaksri@gmail.com>
2017-12-24 14:23:16 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -45,33 +45,6 @@ sudo npm install pdfkit -save -y
 sudo npm install -y
 sudo npm update -y
 
-echo -n "deploy project[y/n] :  "
-read deploy
-while [ "$deploy" != "y" ] && [ "$deploy" != "n" ]
-do
-echo -n "deploy project[y/n] :   "
-read deploy
-done
-
-if [ "$deploy" = 'y' ]
-then
-echo -n "installing pm2"
-sudo npm install pm2 -g
-sudo npm start
-sudo pm2 start npm -- start
-echo "pm2 process completed"
-fi
-
-echo -n "restart nginx [y/n] : "
-read nginx
-if [ "$nginx" = 'y' ]
-then
-echo "processing ..."
-sudo systemctl restart nginx -y
-echo "restart completed"
-fi
-
-
 #sudo yum install policycoreutils-python -y
 #sudo semanage fcontext -a -t httpd_sys_content_t "/var/www/Hobbyforfun(/.*)?" 
 #sudo restorecon -vR /var/www/Hobbyforfun

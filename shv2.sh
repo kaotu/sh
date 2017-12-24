c9632529ad65876a978d63544254b64d2f78c7a8
harit suttisaksri <haritsuttisaksri@gmail.com>
2017-12-24 14:20:47 +0700

    Update shv2.sh

--- shv2.sh
+++ shv2.sh
@@ -71,22 +71,6 @@ sudo systemctl restart nginx -y
 echo "restart completed"
 fi
 
-echo -n "Update ALL SERVICE [y/n] : "
-read nginx
-if [ "$nginx" = 'y' ]
-then
-echo "processing ..."
-sudo yum update -y
-sudo yum update epel-release -y
-sudo yum update nginx -y
-sudo yum update git -y
-sudo yum update curl -y 
-sudo yum update nodejs -y
-sudo yum update npm -y
-sudo yum update php -y
-sudo yum update mariadb mariadb-server -y
-echo "Update completed"
-fi
 
 #sudo yum install policycoreutils-python -y
 #sudo semanage fcontext -a -t httpd_sys_content_t "/var/www/Hobbyforfun(/.*)?" 

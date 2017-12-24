8d848e4efdca5b48bbeef5c1a50021b7cca28597
harit suttisaksri <haritsuttisaksri@gmail.com>
2017-12-24 14:23:37 +0700

    Rename deploy.sh to deployl.sh

---
+++ deployl.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo -n "deploy project[y/n] :  "
+read deploy
+while [ "$deploy" != "y" ] && [ "$deploy" != "n" ]
+do
+echo -n "deploy project[y/n] :   "
+read deploy
+done
+
+if [ "$deploy" = 'y' ]
+then
+echo -n "installing pm2"
+sudo npm install pm2 -g
+sudo npm start
+sudo pm2 start npm -- start
+echo "pm2 process completed"
+fi
+
+echo -n "restart nginx [y/n] : "
+read nginx
+if [ "$nginx" = 'y' ]
+then
+echo "processing ..."
+sudo systemctl restart nginx -y
+echo "restart completed"
+fi

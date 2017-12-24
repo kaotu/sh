48a0d9cfc6b18e800dd49e1534de65ca5a4f39f1
root <patthraporn.tu@mail.kmutt.ac.th>
2017-12-24 09:36:59 +0000

    initial commit

--- install.sh
+++ install.sh
@@ -11,5 +11,13 @@ sudo yum install curl sudo
 sudo curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 sudo sudo yum install nodejs -y
 sudo yum install npm -y
-cd /var/www/
+
+echo -n "Create directory www[y/n] : "
+read www
+if [ "$www" = 'y' ]
+then
+echo -n "Create www"
+sudo mkdir -p /var/www
+echo "Create success"
+fi
 
